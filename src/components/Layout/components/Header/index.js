0faf1456315afd5b1c1d44d595428035d9d2b886
phan_hoang_quan <phanhoangquan123@gmail.com>
2025-05-08 03:59:11 +0700

    Header_Menu

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,15 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faMagnifyingGlass, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import {
+   faCircleQuestion,
+   faCircleXmark,
+   faEarthAsia,
+   faEllipsisVertical,
+   faKeyboard,
+   faMagnifyingGlass,
+   faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 
 import Tippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
@@ -13,7 +21,25 @@ import styles from './Header.module.scss';
 import images from '~/assets/images';
 import AccountItem from '~/components/AccountItem';
 
+import Menu from '~/components/Popper/Menu';
+
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+   {
+      icon: <FontAwesomeIcon icon={faEarthAsia} />,
+      title: 'Tiếng Việt',
+   },
+   {
+      icon: <FontAwesomeIcon icon={faCircleQuestion} />,
+      title: 'Feedback and Help',
+      to: '/feedback',
+   },
+   {
+      icon: <FontAwesomeIcon icon={faKeyboard} />,
+      title: 'Keybroad shortcuts',
+   },
+];
 
 function Header() {
    const [searchResult, setSearchResult] = useState([]);
@@ -56,6 +82,12 @@ function Header() {
                <Button text>Upload</Button>
                <Button primary>Log in</Button>
                <Button outline>Register</Button>
+
+               <Menu items={MENU_ITEMS}>
+                  <button className={cx('more-btn')}>
+                     <FontAwesomeIcon icon={faEllipsisVertical} />
+                  </button>
+               </Menu>
             </div>
          </div>
       </header>

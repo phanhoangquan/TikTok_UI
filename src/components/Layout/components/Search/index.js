f64f83dec5078f344a0936cb0c6b09dd88dc9799
phan_hoang_quan <phanhoangquan123@gmail.com>
2025-05-24 18:54:37 +0700

    Header_axiosfetch

--- src/components/Layout/components/Search/index.js
+++ src/components/Layout/components/Search/index.js
@@ -8,6 +8,7 @@ import { useDebounce } from '~/hooks';
 import classNames from 'classnames/bind';
 import { SearchIcon } from '~/components/Icons';
 import styles from './Search.module.scss';
+import * as searchServices from '~/apiServices/searchServices';
 
 const cx = classNames.bind(styles);
 
@@ -26,17 +27,13 @@ function Search() {
          return;
       }
 
-      setLoading(true);
-
-      fetch(`https://tiktok.fullstack.edu.vn/api/users/search?q=${encodeURIComponent(debounced)}&type=less`)
-         .then((res) => res.json())
-         .then((res) => {
-            setSearchResult(res.data);
-            setLoading(false);
-         })
-         .catch(() => {
-            setLoading(false);
-         });
+      const fetchApi = async () => {
+         setLoading(true);
+         const result = await searchServices.search(debounced);
+         setSearchResult(result);
+         setLoading(false);
+      };
+      fetchApi();
    }, [debounced]);
 
    const handleClickOutside = () => {
